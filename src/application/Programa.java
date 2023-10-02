4582ae1da208e08b6b3407a003f71b797b1bfd76
Leonardo Infante <leosargino@gmail.com>
2023-10-02 16:25:59 -0300

    Criação classe Vendedor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Departamento;
+import model.entities.Vendedor;
+
+import java.util.Date;
 
 public class Programa {
     public static void main(String[] args) {
         Departamento departamento = new Departamento(1,"Books");
 
-        System.out.println(departamento);
+        Vendedor vendedor = new Vendedor(1,"Bob","bob@gmail.com",new Date(),3000,departamento);
+
+        System.out.println(vendedor);
     }
 }
 
